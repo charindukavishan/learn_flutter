55f770e571afb670dda00cbc9057eb639774ad1e
charindukavishan <charindukavishan@gmail.com>
2020-10-15 16:05:28 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(title: 'flutter title'),
+      theme: ThemeData(
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({this.title});
+
+  final String title;
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          leading: IconButton(icon: Icon(Icons.menu), onPressed: () {}),
+          actions: [
+            IconButton(icon: Icon(Icons.search), onPressed: () {}),
+            IconButton(icon: Icon(Icons.more_vert), onPressed: () {}),
+          ],
+          flexibleSpace: Image.asset(
+            "assets/ayruwada.png",
+            fit: BoxFit.cover,
+          ),
+          // bottom: TabBar(
+          //   tabs: [
+          //     Tab(icon: Icon(Icons.directions_car)),
+          //     Tab(icon: Icon(Icons.directions_transit)),
+          //     Tab(icon: Icon(Icons.directions_bike)),
+          //   ],
+          // ),
+        ),
+        // body: TabBarView(
+        //     children: [
+        //       Container(
+        //         width: 200.0,
+        //         child: Text("Body",style: TextStyle(fontSize: 20.0),),
+        //         color: Colors.lightBlueAccent,
+        //       ),
+        //       Icon(Icons.directions_transit),
+        //       Icon(Icons.directions_bike),
+        //     ],
+        //   ),
+        body: Container(
+          margin: EdgeInsets.all(5.0),
+          padding: EdgeInsets.all(20.0),
+          // transform: Matrix4.rotationZ(0.5),
+          // alignment: Alignment.center,
+          width: double.infinity,
+          child: Text('Body'),
+          color: Colors.lightBlueAccent,
+        ),
+      ),
+    );
+  }
+}
+
+class app extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("My App"),
+          leading: IconButton(icon: Icon(Icons.menu), onPressed: () {}),
+          actions: [
+            IconButton(icon: Icon(Icons.search), onPressed: () {}),
+            IconButton(icon: Icon(Icons.more_vert), onPressed: () {}),
+          ],
+          flexibleSpace: Image.asset(
+            "assets/ayruwada.png",
+            fit: BoxFit.cover,
+          ),
+          // bottom: TabBar(
+          //   tabs: [
+          //     Tab(icon: Icon(Icons.directions_car)),
+          //     Tab(icon: Icon(Icons.directions_transit)),
+          //     Tab(icon: Icon(Icons.directions_bike)),
+          //   ],
+          // ),
+        ),
+        // body: TabBarView(
+        //     children: [
+        //       Container(
+        //         width: 200.0,
+        //         child: Text("Body",style: TextStyle(fontSize: 20.0),),
+        //         color: Colors.lightBlueAccent,
+        //       ),
+        //       Icon(Icons.directions_transit),
+        //       Icon(Icons.directions_bike),
+        //     ],
+        //   ),
+        body: Container(
+          margin: EdgeInsets.all(5.0),
+          padding: EdgeInsets.all(20.0),
+          // transform: Matrix4.rotationZ(0.5),
+          // alignment: Alignment.center,
+          width: double.infinity,
+          child: Text('Body'),
+          color: Colors.lightBlueAccent,
+        ),
+      ),
+    );
+  }
+}
