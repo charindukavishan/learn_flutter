578cb2c9ffc7da30c737f6466a7050fc24356173
charindukavishan <charindukavishan@gmail.com>
2020-10-16 20:55:29 +0530

    basic form and save button

--- lib/screens/form.dart
+++ lib/screens/form.dart
@@ -6,32 +6,59 @@ class FormLearn extends StatefulWidget {
 }
 
 class _FormLearnState extends State<FormLearn> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+
   @override
   Widget build(BuildContext context) {
+
+    String _name;
     return Scaffold(
       appBar: AppBar(
         title: Text('Learn Form'),
       ),
       body: SingleChildScrollView(
-          child: Container(
-          margin: const EdgeInsets.all(24.0),
-          child: Column(
-            children: [
-              Container(
-                color: Colors.amber,
-                height: 300,
-              ),
-              Container(
-                color: Colors.deepPurple,
-                height: 300,
-              ),
-              Container(
-                color: Colors.limeAccent,
-                height: 300,
-              ),
-            ],
-          ),
+          child: Form(
+            key: _formKey,
+            child: Container(
+            margin: const EdgeInsets.all(24.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  maxLength: 10,
+                  decoration: InputDecoration(
+                    hintText: "Name",
+                    prefixIcon: Icon(Icons.info)                    
+                    ),
+                  maxLines: 1,
+                  validator: (text){
+                    if(text.isEmpty){
+                      return "name can't be empty";
+                    }
+
+                    return null;
+                  },
+                  onSaved: (text){
+                    _name = text;
+                  },
+                ),
+
+                SizedBox(height: 100,),
+                Container(
+                  child: RaisedButton(
+                    child: Text('Save'),
+                    onPressed: () {
+                      if(_formKey.currentState.validate()){
+                        _formKey.currentState.save();
+                        print(_name);
+                      }
+                    },
+                  )
+                )
+              ],
+            ),
         ),
+          ),
       ),
     );
   }
