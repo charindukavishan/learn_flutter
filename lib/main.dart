375e2b4b0cd303ded4e4791f9a25a90157dd281b
charindukavishan <charindukavishan@gmail.com>
2020-10-16 11:53:01 +0530

    add  device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/screens/second_screen.dart';
+import 'package:device_preview/device_preview.dart';
 
 import 'screens/home_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(DevicePreview(
+    builder: (context) => MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: DevicePreview.of(context).locale, // <--- /!\ Add the locale
+      builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
       home: HomePage( title: "app bar title"),
       theme: ThemeData(
